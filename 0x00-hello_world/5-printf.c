826ed2a3f557951217c259e1627befd498810de1
Omar_Kassem <omarxkassem@gmail.com>
2023-06-16 04:00:59 +0100

    my first commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: a program that is 
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
