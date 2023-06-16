4dd14c40c211d34b101c01e6f172301cf95414c4
Omar_Kassem <omarxkassem@gmail.com>
2023-06-16 03:42:49 +0100

    my first commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * descriprion: this is descriprion
+ * return: always 0 (success)
+*/
+
+int main (void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
