cdd72e62277d412a15f963c01463aa6a4ac79d9b
omarxkassem <100129679+omarxkassem@users.noreply.github.com>
2023-06-30 03:42:19 +0100

    a
    a

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,41 +1,19 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * *_strncpy - copies a string, printing NUL characters if the size of
- *             destination is large enough, or cropping otherwise.
- * @dest: the destination string.
- * @src: the source string.
- * @n: the size of the destination string.
- *
- * Return: a pointer of the resulting string.
+ * _strncpy - copies a string
+ * @dest: destination of the string
+ * @src: source of the string
+ * @n: amount of bytes from src.
+ * Return: the pointer to dest.
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
-	int len_src, counter, null_chars;
+	int i;
 
-	len_src = counter = null_chars = 0;
-
-	while (src[len_src] != '\0')
-		len_src++;
-
-	if (n >= 0)
-	{
-		while (counter < n)
-		{
-			dest[counter] = src[counter];
-			counter++;
-		}
-		if (n - len_src > 0)
-		{
-			null_chars = n - len_src;
-			counter = 0;
-			while (counter < null_chars)
-			{
-				dest[counter + len_src] = '\0';
-				counter++;
-			}
-		}
-	}
-
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	dest[i] = src[i];
+	for ( ; i < n; i++)
+	dest[i] = '\0';
 	return (dest);
 }
